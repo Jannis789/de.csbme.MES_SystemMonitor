cbe50a6ed39d0dcbccaa11931d1cdac613d47859
Jannis Rustige <jannis.rustige@gmail.com>
2025-05-12 20:27:53 +0200

    change folder structure

--- src/window.py
+++ src/window.py
@@ -2,7 +2,7 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 
-@Gtk.Template(resource_path='/de/csbme/MES_SystemMonitor/window.ui')
+@Gtk.Template(resource_path='/de/csbme/MES_SystemMonitor/view/window.ui')
 class MesMonitorWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'MesMonitorWindow'
 
