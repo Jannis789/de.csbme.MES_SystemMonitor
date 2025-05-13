49a4697b06719a5aebef0bf337823b0145e863e2
Jannis Rustige <jannis.rustige@gmail.com>
2025-05-13 18:57:58 +0200

    implement Produktlinien View

--- src/controller/Produktionslinie.py
+++ src/controller/Produktionslinie.py
@@ -1,1 +1,45 @@
-print("test")
+from gi.repository import Gtk, Gio  # Added Gio import
+from gi.repository import GObject
+
+
+class ProduktionslinienController:
+    def __init__(self, column_view: Gtk.ColumnView):
+        self.column_view = column_view
+
+    def fill_column_view(self, produktionslinien: Gio.ListStore):
+
+        self.column_view.set_model(Gtk.SingleSelection(model=produktionslinien))
+
+        factory = Gtk.SignalListItemFactory()
+        factory.connect("setup", self._on_factory_setup)
+        factory.connect("bind", self._on_factory_bind)
+
+        """ Maybe someday we need to solve this with a dynamic Class generation"""
+        column = Gtk.ColumnViewColumn(title="Name", factory=factory)
+        column.set_expand(True)
+        column.set_resizable(True)
+        self.column_view.append_column(column)
+
+    def _on_factory_setup(self, factory, list_item):
+        label = Gtk.Label()
+        label.set_halign(Gtk.Align.START)
+        # label.add_css_class("title-2")
+        list_item.set_child(label)
+
+    def _on_factory_bind(self, factory, list_item):
+        # Bind the label to the item's name property
+        item = list_item.get_item()
+        list_item.get_child().set_text(item.name)
+            
+
+
+class ProduktionslinienModell(GObject.Object):
+    def __init__(self, name: str):
+        super().__init__()
+        self._name = name  # Initialize the name property
+
+    @GObject.Property(type=str)
+    def name(self):
+        return self._name
+
+
--- src/controller/window.py
+++ src/controller/window.py
@@ -1,12 +1,52 @@
 
 from gi.repository import Adw
 from gi.repository import Gtk
+from gi.repository import Gio
+from gi.repository import Gdk
+
+from .Produktionslinie import ProduktionslinienController, ProduktionslinienModell
+
+
+_COLUMN_VIEW_OVERRIDE = """
+.ColumnView > header {
+  font-size: 18px;
+  font-weight: 900;
+}"""
+
 
 @Gtk.Template(resource_path='/de/csbme/MES_SystemMonitor/view/window.ui')
 class MesMonitorWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'MesMonitorWindow'
+    column_view = Gtk.Template.Child()
+    scrolled_window = Gtk.Template.Child()
 
-    label = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.instanciate_column_view()
+        self.load_css()
+    
+    def instanciate_column_view(self):
+        self.produktionslinien_controller = ProduktionslinienController(self.column_view)    
+    
+        """create Dummy data for the column view"""
+        data_model = Gio.ListStore(item_type=ProduktionslinienModell)
+        for line in [
+            ProduktionslinienModell(name="Produktionslinie 1"),
+            ProduktionslinienModell(name="Produktionslinie 2"),
+            ProduktionslinienModell(name="Produktionslinie 3"),
+        ]:
+            data_model.append(line)
+            
+        self.produktionslinien_controller.fill_column_view(data_model)
+        
+    def load_css(self):
+        style = self.get_style_context()
+        priority = Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        provider = Gtk.CssProvider()
+        try:
+            provider.load_from_data(_COLUMN_VIEW_OVERRIDE, -1)
+        except Exception:
+            provider.load_from_data(bytes(_COLUMN_VIEW_OVERRIDE.encode()))
+        display = Gdk.Display.get_default()
+        style.add_provider_for_display(display, provider, priority)
